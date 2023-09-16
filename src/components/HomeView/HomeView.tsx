71d70850c62b151e1893a33108f953a1f6ffcb80
Karolina <karolina.chodorek3@gmail.com>
2023-09-16 09:26:50 +0200

    Add home view component

---
+++ src/components/HomeView/HomeView.tsx
@@ -0,0 +1,12 @@
+import Button from "../Button/Button";
+import StyledHomeView from "./StyledHomeView";
+
+const HomeView: React.FC = () => {
+  return (
+    <StyledHomeView>
+      <Button>Zobacz naszych księgowych</Button>
+    </StyledHomeView>
+  );
+};
+
+export default HomeView;
---
+++ src/components/HomeView/StyledHomeView.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const StyledHomeView = styled.div``;
+
+export default StyledHomeView;
